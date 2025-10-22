e6f5f5205b6d270fb1c5c34b4eed176964da0000
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2025-10-21 20:57:09 -0400

    feat: add candy basket cutting recipe

--- fabric/src/main/java/com/chefmooon/frightsdelight/data/fabric/recipe/CuttingRecipes.java
+++ fabric/src/main/java/com/chefmooon/frightsdelight/data/fabric/recipe/CuttingRecipes.java
@@ -1,8 +1,10 @@
 package com.chefmooon.frightsdelight.data.fabric.recipe;
 
 import com.chefmooon.frightsdelight.common.registry.fabric.FrightsDelightItemsImpl;
+import com.chefmooon.frightsdelight.common.tag.CommonTags;
 import com.chefmooon.frightsdelight.common.utility.TextUtils;
 import com.chefmooon.frightsdelight.data.fabric.builder.CuttingBoardRecipeBuilder;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.resources.ResourceLocation;
@@ -17,6 +19,14 @@ public class CuttingRecipes {
 //                .save(exporter, suffix(RecipeProvider.getSimpleRecipeName(FrightsDelightItemsImpl.UNFIRED_LOLLIPOP_MOLD))); // run once then change common knife tag
         CuttingBoardRecipeBuilder.create(Items.CLAY, Ingredient.of(Items.GLASS_BOTTLE), FrightsDelightItemsImpl.UNFIRED_RING_CANDY_MOLD)
                 .save(exporter, suffix(RecipeProvider.getSimpleRecipeName(FrightsDelightItemsImpl.UNFIRED_RING_CANDY_MOLD)));
+
+        // run once then change common knife tag
+//        CuttingBoardRecipeBuilder.create(FrightsDelightItemsImpl.PUMPKIN_CANDY_BASKET, Ingredient.of(CommonTags.C_TOOLS_KNIVES), Items.PUMPKIN)
+//                .output(BuiltInRegistries.ITEM.get(new ResourceLocation("farmersdelight", "rope")))
+//                .save(exporter, suffix(RecipeProvider.getConversionRecipeName(Items.PUMPKIN, FrightsDelightItemsImpl.PUMPKIN_CANDY_BASKET)));
+//        CuttingBoardRecipeBuilder.create(FrightsDelightItemsImpl.MELON_CANDY_BASKET, Ingredient.of(CommonTags.C_TOOLS_KNIVES), Items.MELON)
+//                .output(BuiltInRegistries.ITEM.get(new ResourceLocation("farmersdelight", "rope")))
+//                .save(exporter, suffix(RecipeProvider.getConversionRecipeName(Items.MELON, FrightsDelightItemsImpl.MELON_CANDY_BASKET)));
     }
 
     private static ResourceLocation suffix(String string) {
