e216ea2a20db0e48db2b4a63a580d6fb880a0c4f
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2025-10-22 11:26:47 -0400

    feat: rei info for new items/blocks

--- fabric/src/main/java/com/chefmooon/frightsdelight/integration/rei/fabric/ClientREIPlugin.java
+++ fabric/src/main/java/com/chefmooon/frightsdelight/integration/rei/fabric/ClientREIPlugin.java
@@ -6,7 +6,10 @@ import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.common.util.EntryIngredients;
 import me.shedaniel.rei.plugin.common.displays.DefaultInformationDisplay;
+import mezz.jei.api.constants.VanillaTypes;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
 
 import java.util.List;
 
@@ -16,5 +19,35 @@ public class ClientREIPlugin implements REIClientPlugin {
     public void registerDisplays(DisplayRegistry registry) {
         registry.add(DefaultInformationDisplay.createFromEntries(EntryIngredients.ofItems(List.of(FrightsDelightItemsImpl.SOUL_BERRY)), Component.translatable("item.frightsdelight.soul_berry")).lines(TextUtils.getTranslatable("rei.info.soul_berry")));
         registry.add(DefaultInformationDisplay.createFromEntries(EntryIngredients.ofItems(List.of(FrightsDelightItemsImpl.WITHER_BERRY)), Component.translatable("item.frightsdelight.wither_berry")).lines(TextUtils.getTranslatable("rei.info.wither_berry")));
+
+        List<ItemLike> lollipopMoldStacks = List.of(
+                FrightsDelightItemsImpl.LOLLIPOP_MOLD,
+                FrightsDelightItemsImpl.ROTTEN_FLESH_LOLLIPOP,
+                FrightsDelightItemsImpl.SLIMEAPPLE_LOLLIPOP,
+                FrightsDelightItemsImpl.SPIDEREYE_LOLLIPOP,
+                FrightsDelightItemsImpl.GHASTTEAR_LOLLIPOP,
+                FrightsDelightItemsImpl.SOUL_BERRY_LOLLIPOP,
+                FrightsDelightItemsImpl.WITHER_BERRY_LOLLIPOP,
+                FrightsDelightItemsImpl.COBWEB_LOLLIPOP
+        );
+        registry.add(DefaultInformationDisplay.createFromEntries(EntryIngredients.ofItems(lollipopMoldStacks), Component.translatable("block.frightsdelight.lollipop_mold")).lines(TextUtils.getTranslatable("rei.info.lollipop_mold")));
+
+        List<ItemLike> ringCandyMoldStacks = List.of(
+                FrightsDelightItemsImpl.RING_CANDY_MOLD,
+                FrightsDelightItemsImpl.ROTTEN_FLESH_RING_CANDY,
+                FrightsDelightItemsImpl.SLIMEAPPLE_RING_CANDY,
+                FrightsDelightItemsImpl.SPIDEREYE_RING_CANDY,
+                FrightsDelightItemsImpl.GHASTTEAR_RING_CANDY,
+                FrightsDelightItemsImpl.SOUL_BERRY_RING_CANDY,
+                FrightsDelightItemsImpl.WITHER_BERRY_RING_CANDY,
+                FrightsDelightItemsImpl.COBWEB_RING_CANDY
+        );
+        registry.add(DefaultInformationDisplay.createFromEntries(EntryIngredients.ofItems(ringCandyMoldStacks), Component.translatable("block.frightsdelight.ring_candy_mold")).lines(TextUtils.getTranslatable("rei.info.ring_candy_mold")));
+
+        List<ItemLike> candyBasketStack = List.of(
+                FrightsDelightItemsImpl.PUMPKIN_CANDY_BASKET,
+                FrightsDelightItemsImpl.MELON_CANDY_BASKET
+        );
+        registry.add(DefaultInformationDisplay.createFromEntries(EntryIngredients.ofItems(candyBasketStack), Component.translatable("block.frightsdelight.pumpkin_candy_basket")).lines(TextUtils.getTranslatable("rei.info.candy_basket")));
     }
 }
