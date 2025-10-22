43138346d96ffde0e2b784670650de00bf826db9
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2025-10-22 10:48:59 -0400

    feat: jei info for new items/blocks

--- fabric/src/main/java/com/chefmooon/frightsdelight/integration/jei/fabric/JEIPluginImpl.java
+++ fabric/src/main/java/com/chefmooon/frightsdelight/integration/jei/fabric/JEIPluginImpl.java
@@ -11,6 +11,8 @@ import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.List;
+
 @JeiPlugin
 @MethodsReturnNonnullByDefault
 @SuppressWarnings("unused")
@@ -25,5 +27,35 @@ public class JEIPluginImpl implements IModPlugin {
     public void registerRecipes(IRecipeRegistration registration) {
         registration.addIngredientInfo(new ItemStack(FrightsDelightItemsImpl.SOUL_BERRY), VanillaTypes.ITEM_STACK, TextUtils.getTranslatable("rei.info.soul_berry"));
         registration.addIngredientInfo(new ItemStack(FrightsDelightItemsImpl.WITHER_BERRY), VanillaTypes.ITEM_STACK, TextUtils.getTranslatable("rei.info.wither_berry"));
+
+        List<ItemStack> lollipopMoldStacks = List.of(
+                FrightsDelightItemsImpl.LOLLIPOP_MOLD.getDefaultInstance(),
+                FrightsDelightItemsImpl.ROTTEN_FLESH_LOLLIPOP.getDefaultInstance(),
+                FrightsDelightItemsImpl.SLIMEAPPLE_LOLLIPOP.getDefaultInstance(),
+                FrightsDelightItemsImpl.SPIDEREYE_LOLLIPOP.getDefaultInstance(),
+                FrightsDelightItemsImpl.GHASTTEAR_LOLLIPOP.getDefaultInstance(),
+                FrightsDelightItemsImpl.SOUL_BERRY_LOLLIPOP.getDefaultInstance(),
+                FrightsDelightItemsImpl.WITHER_BERRY_LOLLIPOP.getDefaultInstance(),
+                FrightsDelightItemsImpl.COBWEB_LOLLIPOP.getDefaultInstance()
+        );
+        registration.addIngredientInfo(lollipopMoldStacks, VanillaTypes.ITEM_STACK, TextUtils.getTranslatable("rei.info.lollipop_mold"));
+
+        List<ItemStack> ringCandyMoldStacks = List.of(
+                FrightsDelightItemsImpl.RING_CANDY_MOLD.getDefaultInstance(),
+                FrightsDelightItemsImpl.ROTTEN_FLESH_RING_CANDY.getDefaultInstance(),
+                FrightsDelightItemsImpl.SLIMEAPPLE_RING_CANDY.getDefaultInstance(),
+                FrightsDelightItemsImpl.SPIDEREYE_RING_CANDY.getDefaultInstance(),
+                FrightsDelightItemsImpl.GHASTTEAR_RING_CANDY.getDefaultInstance(),
+                FrightsDelightItemsImpl.SOUL_BERRY_RING_CANDY.getDefaultInstance(),
+                FrightsDelightItemsImpl.WITHER_BERRY_RING_CANDY.getDefaultInstance(),
+                FrightsDelightItemsImpl.COBWEB_RING_CANDY.getDefaultInstance()
+        );
+        registration.addIngredientInfo(ringCandyMoldStacks, VanillaTypes.ITEM_STACK, TextUtils.getTranslatable("rei.info.ring_candy_mold"));
+
+        List<ItemStack> candyBasketStack = List.of(
+                FrightsDelightItemsImpl.PUMPKIN_CANDY_BASKET.getDefaultInstance(),
+                FrightsDelightItemsImpl.MELON_CANDY_BASKET.getDefaultInstance()
+        );
+        registration.addIngredientInfo(candyBasketStack, VanillaTypes.ITEM_STACK, TextUtils.getTranslatable("rei.info.candy_basket"));
     }
 }
