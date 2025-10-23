34db73d13538e20b86b411d0369b56fd2a579210
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2025-10-23 17:32:48 -0400

    feat: add ring candy to compostable registry

--- common/src/main/java/com/chefmooon/frightsdelight/common/CommonSetup.java
+++ common/src/main/java/com/chefmooon/frightsdelight/common/CommonSetup.java
@@ -1,7 +1,6 @@
 package com.chefmooon.frightsdelight.common;
 
 import com.chefmooon.frightsdelight.common.registry.FrightsDelightItems;
-import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.block.ComposterBlock;
 
@@ -11,19 +10,27 @@ public class CommonSetup {
     }
 
     public static void registerCompostables() {
-        CompostingChanceRegistry.INSTANCE.add(BuiltInRegistries.ITEM.get(FrightsDelightItems.SOUL_BERRY).get().value(), 0.3F);
-        CompostingChanceRegistry.INSTANCE.add(BuiltInRegistries.ITEM.get(FrightsDelightItems.WITHER_BERRY).get().value(), 0.3F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.SOUL_BERRY).get().value(), 0.3F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.WITHER_BERRY).get().value(), 0.3F);
 
-        CompostingChanceRegistry.INSTANCE.add(BuiltInRegistries.ITEM.get(FrightsDelightItems.APPLE_SLIME).get().value(), 0.65F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.ROTTEN_FLESH_RING_CANDY).get().value(), 0.5F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.SLIMEAPPLE_RING_CANDY).get().value(), 0.5F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.SPIDEREYE_RING_CANDY).get().value(), 0.5F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.GHASTTEAR_RING_CANDY).get().value(), 0.5F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.SOUL_BERRY_RING_CANDY).get().value(), 0.5F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.WITHER_BERRY_RING_CANDY).get().value(), 0.5F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.COBWEB_RING_CANDY).get().value(), 0.5F);
 
-        CompostingChanceRegistry.INSTANCE.add(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_SOUL_BERRY).get().value(), 0.85F);
-        CompostingChanceRegistry.INSTANCE.add(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_WITHER_BERRY).get().value(), 0.85F);
-        CompostingChanceRegistry.INSTANCE.add(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_ROTTEN_FLESH).get().value(), 0.85F);
-        CompostingChanceRegistry.INSTANCE.add(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_SPIDER_EYE).get().value(), 0.85F);
-        CompostingChanceRegistry.INSTANCE.add(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_SLIMEAPPLE).get().value(), 0.85F);
-        CompostingChanceRegistry.INSTANCE.add(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_SLIME).get().value(), 0.85F);
-        CompostingChanceRegistry.INSTANCE.add(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_COBWEB).get().value(), 0.85F);
-        CompostingChanceRegistry.INSTANCE.add(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_GHAST_TEAR).get().value(), 0.85F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.APPLE_SLIME).get().value(), 0.65F);
+
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_SOUL_BERRY).get().value(), 0.85F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_WITHER_BERRY).get().value(), 0.85F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_ROTTEN_FLESH).get().value(), 0.85F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_SPIDER_EYE).get().value(), 0.85F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_SLIMEAPPLE).get().value(), 0.85F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_SLIME).get().value(), 0.85F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_COBWEB).get().value(), 0.85F);
+        ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.COOKIE_GHAST_TEAR).get().value(), 0.85F);
 
         ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.ROTTEN_FLESH_PIE_SLICE).get().value(), 0.85F);
         ComposterBlock.COMPOSTABLES.put(BuiltInRegistries.ITEM.get(FrightsDelightItems.SLIMEAPPLE_PIE_SLICE).get().value(), 0.85F);
