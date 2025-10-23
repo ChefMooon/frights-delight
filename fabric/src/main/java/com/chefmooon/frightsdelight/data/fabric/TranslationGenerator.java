65f6716ba1de640adb4de57db763cdcfa4f200d5
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2025-10-23 16:13:01 -0400

    refactor: candy basket translation base

--- fabric/src/main/java/com/chefmooon/frightsdelight/data/fabric/TranslationGenerator.java
+++ fabric/src/main/java/com/chefmooon/frightsdelight/data/fabric/TranslationGenerator.java
@@ -149,8 +149,8 @@ public class TranslationGenerator extends FabricLanguageProvider {
         translationBuilder.add(FrightsDelightItemsImpl.WITHER_BERRY_RING_CANDY, "Wither Berry Ring Candy");
         translationBuilder.add(FrightsDelightItemsImpl.COBWEB_RING_CANDY, "Cobweb Ring Candy");
 
-        translationBuilder.add(FrightsDelightItemsImpl.PUMPKIN_CANDY_BASKET, "Pumpkin Candy Basket");
-        translationBuilder.add(FrightsDelightItemsImpl.MELON_CANDY_BASKET, "Melon Candy Basket");
+        translationBuilder.add(FrightsDelightBlocksImpl.PUMPKIN_CANDY_BASKET, "Pumpkin Candy Basket");
+        translationBuilder.add(FrightsDelightBlocksImpl.MELON_CANDY_BASKET, "Melon Candy Basket");
 
 //        translationBuilder.add(FrightsDelightItemsImpl.CREEPER_CANDY_BASKET, "Creeper Candy Basket");
 //        translationBuilder.add(FrightsDelightItemsImpl.SKELETON_CANDY_BASKET, "Skeleton Candy Basket");
