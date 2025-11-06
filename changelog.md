430dfd7e1e1f0e7dd382a657b65b2f678f54ba2c
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2025-11-06 16:11:49 -0500

    update: architectury loom 1.13
    - implement JEI integration
    - update changelog with eiv info

--- changelog.md
+++ changelog.md
@@ -9,7 +9,16 @@ This mod is compatible with Minecraft 1.21.5 and Farmer's Delight Refabricated
 
 ## V1.4.1
 
-Includes important fixes for the 1.21.5 Fabric release.
+### Added
+
+- [1.21.5+] Extended ItemView (EIV) support
+
+### Fixed
+
+- [1.21.5+] Fixed Rotten Flesh Punchbowl not dropping Punch when broken with a knife
+- [1.21.5+] Fixed Pie block loot table at 3/4 bites not dropping Pie Slice
+- [1.21.5+] Fixed Effects not applying correctly
+- [1.20.1+] Fixed Candy Mold hardening time after first craft
 
 ## V1.4.0
 
--- fabric/src/main/java/com/chefmooon/frightsdelight/integration/jei/fabric/JEIPluginImpl.java
+++ fabric/src/main/java/com/chefmooon/frightsdelight/integration/jei/fabric/JEIPluginImpl.java
@@ -1,30 +1,62 @@
-//package com.chefmooon.frightsdelight.integration.jei.fabric;
-//
-//import com.chefmooon.frightsdelight.common.registry.fabric.FrightsDelightItemsImpl;
-//import com.chefmooon.frightsdelight.common.utility.TextUtils;
-//import com.chefmooon.frightsdelight.integration.jei.JEIPlugin;
-//import mezz.jei.api.IModPlugin;
-//import mezz.jei.api.JeiPlugin;
-//import mezz.jei.api.constants.VanillaTypes;
-//import mezz.jei.api.registration.IRecipeRegistration;
-//import net.minecraft.MethodsReturnNonnullByDefault;
-//import net.minecraft.resources.ResourceLocation;
-//import net.minecraft.world.item.ItemStack;
-//
- //  ----- Disabled due to JEI not being available in Fabric 1.21.5 -----
-//@JeiPlugin
-//@MethodsReturnNonnullByDefault
-//@SuppressWarnings("unused")
-//public class JEIPluginImpl implements IModPlugin {
-//
-//    @Override
-//    public ResourceLocation getPluginUid() {
-//        return JEIPlugin.ID;
-//    }
-//
-//    @Override
-//    public void registerRecipes(IRecipeRegistration registration) {
-//        registration.addIngredientInfo(new ItemStack(FrightsDelightItemsImpl.SOUL_BERRY.get()), VanillaTypes.ITEM_STACK, TextUtils.getTranslatable("rei.info.soul_berry"));
-//        registration.addIngredientInfo(new ItemStack(FrightsDelightItemsImpl.WITHER_BERRY.get()), VanillaTypes.ITEM_STACK, TextUtils.getTranslatable("rei.info.wither_berry"));
-//    }
-//}
+package com.chefmooon.frightsdelight.integration.jei.fabric;
+
+import com.chefmooon.frightsdelight.common.registry.fabric.FrightsDelightItemsImpl;
+import com.chefmooon.frightsdelight.common.utility.TextUtils;
+import com.chefmooon.frightsdelight.integration.jei.JEIPlugin;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+
+@JeiPlugin
+@MethodsReturnNonnullByDefault
+@SuppressWarnings("unused")
+public class JEIPluginImpl implements IModPlugin {
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return JEIPlugin.ID;
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        registration.addIngredientInfo(new ItemStack(FrightsDelightItemsImpl.SOUL_BERRY.get()), VanillaTypes.ITEM_STACK, TextUtils.getTranslatable("rei.info.soul_berry"));
+        registration.addIngredientInfo(new ItemStack(FrightsDelightItemsImpl.WITHER_BERRY.get()), VanillaTypes.ITEM_STACK, TextUtils.getTranslatable("rei.info.wither_berry"));
+
+        List<ItemStack> lollipopMoldStacks = List.of(
+                FrightsDelightItemsImpl.LOLLIPOP_MOLD.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.ROTTEN_FLESH_LOLLIPOP.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.SLIMEAPPLE_LOLLIPOP.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.SPIDEREYE_LOLLIPOP.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.GHASTTEAR_LOLLIPOP.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.SOUL_BERRY_LOLLIPOP.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.WITHER_BERRY_LOLLIPOP.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.COBWEB_LOLLIPOP.get().getDefaultInstance()
+        );
+        registration.addIngredientInfo(lollipopMoldStacks, VanillaTypes.ITEM_STACK, TextUtils.getTranslatable("rei.info.lollipop_mold"));
+
+        List<ItemStack> ringCandyMoldStacks = List.of(
+                FrightsDelightItemsImpl.RING_CANDY_MOLD.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.ROTTEN_FLESH_RING_CANDY.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.SLIMEAPPLE_RING_CANDY.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.SPIDEREYE_RING_CANDY.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.GHASTTEAR_RING_CANDY.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.SOUL_BERRY_RING_CANDY.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.WITHER_BERRY_RING_CANDY.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.COBWEB_RING_CANDY.get().getDefaultInstance()
+        );
+        registration.addIngredientInfo(ringCandyMoldStacks, VanillaTypes.ITEM_STACK, TextUtils.getTranslatable("rei.info.ring_candy_mold"));
+
+        List<ItemStack> candyBasketStack = List.of(
+                FrightsDelightItemsImpl.PUMPKIN_CANDY_BASKET.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.JACK_O_LANTERN_CANDY_BASKET.get().getDefaultInstance(),
+                FrightsDelightItemsImpl.MELON_CANDY_BASKET.get().getDefaultInstance()
+        );
+        registration.addIngredientInfo(candyBasketStack, VanillaTypes.ITEM_STACK, TextUtils.getTranslatable("rei.info.candy_basket"));
+    }
+}
