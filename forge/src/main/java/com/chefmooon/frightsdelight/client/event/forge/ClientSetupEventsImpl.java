a27eb034c683b235143191e7c2a4f8bba7d7a8f5
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2025-11-24 11:01:55 -0500

    fix: midnightlib v1.9.0 crash
    - update forge midnight lib implementation
    - update changelog

--- forge/src/main/java/com/chefmooon/frightsdelight/client/event/forge/ClientSetupEventsImpl.java
+++ forge/src/main/java/com/chefmooon/frightsdelight/client/event/forge/ClientSetupEventsImpl.java
@@ -6,39 +6,17 @@ import com.chefmooon.frightsdelight.client.renderer.forge.LollipopMoldBlockEntit
 import com.chefmooon.frightsdelight.common.registry.FrightsDelightParticleTypes;
 import com.chefmooon.frightsdelight.common.registry.forge.FrightsDelightBlockEntitiesImpl;
 import com.chefmooon.frightsdelight.common.registry.forge.FrightsDelightEntityTypesImpl;
-import eu.midnightdust.lib.config.MidnightConfig;
-import eu.midnightdust.lib.util.MidnightColorUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.ThrownItemRenderer;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = FrightsDelight.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientSetupEventsImpl {
-    public static void registerClientTick(TickEvent.ClientTickEvent event) {
-        MidnightColorUtil.tick();
-    }
-
-    @SubscribeEvent
-    public static void onPostInit(FMLClientSetupEvent event) {
-        MinecraftForge.EVENT_BUS.addListener(ClientSetupEventsImpl::registerClientTick);
-        ModList.get().forEachModContainer((modid, modContainer) -> {
-            if (MidnightConfig.configClass.containsKey(modid)) {
-                modContainer.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
-                        new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> MidnightConfig.getScreen(parent, modid)));
-            }
-        });
-
-    }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void registerParticles(RegisterParticleProvidersEvent event) {
